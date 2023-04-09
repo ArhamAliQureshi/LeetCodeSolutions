974f86ceb74e0308b84aba724c79aa371015e89f
Arham Ali Qureshi <arhamaliqureshi@gmail.com>
2023-04-09 14:50:06 +0000

    Karat Interview Solution added

---
+++ Interviews/KaratInterview/ActiveConnectionsFinder.js
@@ -0,0 +1,115 @@
+"use strict";
+
+/*
+You are analyzing data for Aquaintly, a hot new social network.
+
+On Aquaintly, connections are always symmetrical. If a user Alice is connected to Bob, then Bob is also connected to Alice.
+
+You are given a sequential log of CONNECT and DISCONNECT events of the following form:
+- This event connects users Alice and Bob: ["CONNECT", "Alice", "Bob"]
+- This event disconnects the same users: ["DISCONNECT", "Bob", "Alice"] (order of users does not matter)
+
+We want to separate users based on their popularity (number of connections). To do this, write a function that takes in the event log and a number N and returns two collections:
+[Users with less than N connections], [Users with N or more connections]
+
+Example:
+events = [
+    ["CONNECT","Alice","Bob"],
+    ["DISCONNECT","Bob","Alice"],
+    ["CONNECT","Alice","Charlie"],
+    ["CONNECT","Dennis","Bob"],
+    ["CONNECT","Pam","Dennis"],
+    ["DISCONNECT","Pam","Dennis"],
+    ["CONNECT","Pam","Dennis"],
+    ["CONNECT","Edward","Bob"],
+    ["CONNECT","Dennis","Charlie"],
+    ["CONNECT","Alice","Nicole"],
+    ["CONNECT","Pam","Edward"],
+    ["DISCONNECT","Dennis","Charlie"],
+    ["CONNECT","Dennis","Edward"],
+    ["CONNECT","Charlie","Bob"]
+]
+
+Using a target of 3 connections, the expected results are:
+Users with less than 3 connections: ["Alice", "Charlie", "Pam", "Nicole"]
+Users with 3 or more connections: ["Dennis", "Bob", "Edward"]
+
+All test cases:
+grouping(events, 3) => [["Alice", "Charlie", "Pam", "Nicole"], ["Dennis", "Bob", "Edward"]]
+grouping(events, 1) => [[], ["Alice", "Charlie", "Dennis", "Bob", "Pam", "Edward", "Nicole"]]
+grouping(events, 10) => [["Alice", "Charlie", "Dennis", "Bob", "Pam", "Edward", "Nicole"], []]
+Complexity Variables:
+E = number of events
+U = number of users
+
+*/
+let data = [
+  ['CONNECT','Alice','Bob'],  
+  ['DISCONNECT','Bob','Alice'],
+  ['CONNECT','Alice','Charlie'],
+  ['CONNECT','Dennis','Bob'],
+  ['CONNECT','Pam','Dennis'],
+  ['DISCONNECT','Pam','Dennis'],
+  ['CONNECT','Pam','Dennis'],
+  ['CONNECT','Edward','Bob'],
+  ['CONNECT','Dennis','Charlie'],
+  ['CONNECT','Alice','Nicole'],
+  ['CONNECT','Pam','Edward'],
+  ['DISCONNECT','Dennis','Charlie'],
+  ['CONNECT','Dennis','Edward'],
+  ['CONNECT','Charlie','Bob']
+]
+
+console.log(main(data, 3));
+
+
+function main(events, target){
+  let resultObj = parseData(events)   
+  return findConnection(resultObj, target)
+}
+
+
+function parseData(events){
+  let resultObj = {};
+  events.map(item=>{
+    if(item[0]==='CONNECT'){
+      if(!resultObj[item[1]]){
+        resultObj[item[1]]=[]
+      }
+      if(!resultObj[item[2]]){
+        resultObj[item[2]]=[]
+      }
+      resultObj[item[1]].push(item[2])
+      resultObj[item[2]].push(item[1])
+    }
+    else{
+      let index1 = resultObj[item[1]].indexOf(item[2]);
+      let index2 = resultObj[item[2]].indexOf(item[1]);
+     
+      resultObj[item[1]].splice(index1, 1);
+      resultObj[item[2]].splice(index2, 1); 
+      
+    }
+  })
+  return resultObj;
+  
+}
+
+function findConnection(obj, targetNumber){
+  let keys = Object.keys(obj)
+  let lessConnections = [];
+  let moreConnections = [];
+  keys.map(item=>{    
+    if(obj[item].length>=targetNumber){
+      moreConnections.push(item);
+    }
+    else{
+      lessConnections.push(item)
+    }
+  })
+  
+  return {lessConnections , moreConnections}
+}
+
+
+
